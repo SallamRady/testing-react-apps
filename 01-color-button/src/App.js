8ea07e05f705cd958161c8b7ec484bf8040d6376
SallamRady <sallamrady@gmail.com>
2024-12-14 12:46:24 +0200

    add color button testing in normal case in onclick case

--- 01-color-button/src/App.js
+++ 01-color-button/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // Vars and States
+  const [buttonColor, setButtonColor] = useState("red");
+
+  // Methods
+  function toggleButtonColor() {
+    setButtonColor(buttonColor === "red" ? `blue` : `red`);
+  }
+
+  // return ui
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={toggleButtonColor}
+        style={{ backgroundColor: buttonColor }}
+      >
+        Change to {buttonColor === "red" ? `blue` : `red`}
+      </button>
     </div>
   );
 }
--- 01-color-button/src/App.test.js
+++ 01-color-button/src/App.test.js
@@ -1,10 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initial color,check after clicked", () => {
+  // render component which have elements you will test
   render(<App />);
-  const linkElement = screen.getByRole("link", {
-    name: /learn react/i,
-  });
-  expect(linkElement).toBeInTheDocument();
+
+  // select element with role button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expext background of button to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // add click event effect to color button
+  fireEvent.click(colorButton);
+
+  // expext background of button to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect button text updated to 'Change to red'
+  expect(colorButton).toHaveTextContent("Change to red");
 });

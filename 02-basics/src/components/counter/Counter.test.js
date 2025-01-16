03cc539558eab1261374371fdd5a8c029bf46533
SallamRady <sallamrady@gmail.com>
2025-01-16 14:50:58 +0200

    groupping using describe and skip and only keywords

--- 02-basics/src/components/counter/Counter.test.js
+++ 02-basics/src/components/counter/Counter.test.js
@@ -1,32 +1,44 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Counter from "./Counter";
 
-test("Up counter button test", () => {
-  // render our component
-  render(<Counter />);
-  // select our elements from dom
-  const counter = screen.getByTestId("counter");
-  const up = screen.getByTestId("counter-up");
+describe("counter actions tests", () => {
+  test.skip("counter initial value to be 0", () => {
+    // render our component
+    render(<Counter />);
+    // select our elements from dom
+    const counter = screen.getByTestId("counter");
 
-  // make sure initial valie of counter is zero
-  expect(counter).toHaveTextContent("0");
+    // make sure initial valie of counter is zero
+    expect(counter).toHaveTextContent("0");
+  });
 
-  // check when click on increase btn -> counter will increase
-  fireEvent.click(up);
-  expect(counter).toHaveTextContent("1");
-});
+  test.only("Up counter button test", () => {
+    // render our component
+    render(<Counter />);
+    // select our elements from dom
+    const counter = screen.getByTestId("counter");
+    const up = screen.getByTestId("counter-up");
 
-test("down counter button test", () => {
-  // render our component
-  render(<Counter />);
-  // select our elements from dom
-  const counter = screen.getByTestId("counter");
-  const down = screen.getByTestId("counter-down");
+    // make sure initial valie of counter is zero
+    expect(counter).toHaveTextContent("0");
 
-  // make sure initial valie of counter is zero
-  expect(counter).toHaveTextContent("0");
+    // check when click on increase btn -> counter will increase
+    fireEvent.click(up);
+    expect(counter).toHaveTextContent("1");
+  });
 
-  // check when click on decrease btn -> counter will decrease
-  fireEvent.click(down);
-  expect(counter.innerHTML).toMatch("-1");
+  test("down counter button test", () => {
+    // render our component
+    render(<Counter />);
+    // select our elements from dom
+    const counter = screen.getByTestId("counter");
+    const down = screen.getByTestId("counter-down");
+
+    // make sure initial valie of counter is zero
+    expect(counter).toHaveTextContent("0");
+
+    // check when click on decrease btn -> counter will decrease
+    fireEvent.click(down);
+    expect(counter.innerHTML).toMatch("-1");
+  });
 });

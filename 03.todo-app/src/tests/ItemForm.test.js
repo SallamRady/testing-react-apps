9fce8a455ee42f5ec27eee3c7925e88e2baa3986
SallamRady <sallamrady@gmail.com>
2025-01-27 17:17:10 +0300

    implement test case num 1,2 in ItemForm Component

--- 03.todo-app/src/tests/ItemForm.test.js
+++ 03.todo-app/src/tests/ItemForm.test.js
@@ -1,7 +1,40 @@
+import { render, screen } from "@testing-library/react";
+
+import userEvent from "@testing-library/user-event";
+
+import { ItemForm } from "../components/ItemForm";
+
 // ** test cases ** //
-// test case#1 - form rendered correctly
+// test case#1 - item form rendered correctly
+test("Testcase #1 item form rendered correctly", () => {
+  // render our component
+  render(<ItemForm />);
+  // declare element
+  const formTitle = screen.getByTestId("app-title");
+  const taskName = screen.getByTestId("task-name");
+  const addBtn = screen.getByTestId("task-add-btn");
+
+  // check elements in the doc
+  expect(formTitle).toBeInTheDocument();
+  expect(taskName).toBeInTheDocument();
+  expect(addBtn).toBeInTheDocument();
+});
 
 // test case#2 - when user type ,input value will change
+test("Testcase#2 - when user type ,input value will change", async () => {
+  // render our component
+  render(<ItemForm />);
+  // declare element
+  const taskName = screen.getByTestId("task-name");
+
+  // check
+  /**
+   * userEvent.type simulate when user type on input filed note syntax
+   * userEvent.type(element,'string which u simulate you write')
+   */
+  await userEvent.type(taskName, "task 1");
+  expect(taskName).toHaveValue("task 1");
+});
 
 // test case#3 - check error message displayed when input value is empty
 

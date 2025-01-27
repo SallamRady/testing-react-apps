97a7200f6fa360c9975cb2b921722686e95bd88d
SallamRady <sallamrady@gmail.com>
2025-01-27 16:54:31 +0300

    set our testcases in ItemForm components

---
+++ 03.todo-app/src/tests/ItemForm.test.js
@@ -0,0 +1,12 @@
+// ** test cases ** //
+// test case#1 - form rendered correctly
+
+// test case#2 - when user type ,input value will change
+
+// test case#3 - check error message displayed when input value is empty
+
+// test case#4 - check if item added to list when user click on add button
+
+// test case#5 - check if input cleared after item added
+
+// test case#6 - check if item deleted to list when user click on delete button

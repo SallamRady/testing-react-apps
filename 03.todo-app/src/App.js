0012387926ef81e8c4e16e41d67d5d1feccd3ca7
SallamRady <sallamrady@gmail.com>
2025-01-27 16:53:58 +0300

    setup todo app

--- 03.todo-app/src/App.js
+++ 03.todo-app/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {ItemList} from "./components/ItemList";
+import {ItemForm} from "./components/ItemForm";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    // {id: 1, name: "Task 1", done: false},
+    // {id: 2, name: "Task 2", done: false},
+    // {id: 3, name: "Task 3", done: false},
+    // {id: 4, name: "Task 4", done: false},
+  ]);
+const onAddTask = (task) => {
+    if(task.name !== "") {
+        const id = tasks.length + 1;
+        const newTask = {id, ...task};
+        setTasks([...tasks, newTask]);
+    }
+  }
+  const onDeleteTask = (task) => {
+    setTasks(tasks.filter((t) => t.id !== task.id));
+  };
+  const onDoneTask = (task) => {
+    setTasks(tasks.map((t) => t.id === task.id ? {...task, done: !task.done} : t));
+   }
+
+   const onEditTask = (task) => {
+    setTasks(tasks.map((t) => t.id === task.id ? {...task} : t));
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ItemForm onAddTask={onAddTask}/>
+      <ItemList onDeleteTask={onDeleteTask} tasks={tasks} onEditTask={onEditTask}  onDoneTask={onDoneTask}/>
     </div>
   );
 }

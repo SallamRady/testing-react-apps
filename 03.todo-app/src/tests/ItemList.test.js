55d96c2c8d1d93fea05286438f9033c8af62a5b5
SallamRady <sallamrady@gmail.com>
2025-01-28 09:59:35 +0300

    Item List test case 2 and 3

--- 03.todo-app/src/tests/ItemList.test.js
+++ 03.todo-app/src/tests/ItemList.test.js
@@ -9,10 +9,27 @@ test("testcase #1 - check if component render  tasks when tasks are passed", () 
   // render our component
   render(<ItemList tasks={[]} />);
 
-  const tasks = screen.queryAllByTestId('task');
+  const tasks = screen.queryAllByTestId("task");
 
-  expect(tasks).toHaveLength(0)
+  expect(tasks).toHaveLength(0);
 });
 
-
 // testcase #2 - check if component render 1 task when 1 task is passed
+test("testcase #2 - check if component render 1 task when 1 task is passed", () => {
+  // render our component
+  render(<ItemList tasks={[{ id: 1, name: "Task 1", done: false }]} />);
+
+  const tasks = screen.queryAllByTestId("task");
+
+  expect(tasks).toHaveLength(1);
+});
+
+// testcase #3 check if there is no data no data found message appear
+test("testcase #3 check if there is no data no data found message appear", async () => {
+  // render our component
+  render(<ItemList tasks={[]} />);
+
+  const noData = await screen.findByTestId("no-tasks");
+
+  expect(noData).toBeInTheDocument();
+});
